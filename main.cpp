6531a590a6556ca215687b83459eaeba7d29896b
Giang Le <gianghl2@illinois.edu>
2021-01-31 06:48:18 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  cs225
+//
+//  Created by Giang Ha Le on 8/28/20.
+//  Copyright © 2020 Giang Ha Le. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+      int number = 88;    // Declare an int variable and assign an initial value
+      int * pNumber;      // Declare a pointer variable pointing to an int (or int pointer)
+      pNumber = &number;  // assign the address of the variable number to pointer pNumber
+    
+      cout << pNumber << endl;  // Print content of pNumber (0x22ccf0)
+      cout << &number << endl;  // Print address of number (0x22ccf0)
+      cout << *pNumber << endl; // Print value pointed to by pNumber (88)
+      cout << number << endl;   // Print value of number (88)
+    
+      *pNumber = 99;            // Re-assign value pointed to by pNumber
+      cout << pNumber << endl;  // Print content of pNumber (0x22ccf0)
+      cout << &number << endl;  // Print address of number (0x22ccf0)
+      cout << *pNumber << endl; // Print value pointed to by pNumber (99)
+      cout << number << endl;   // Print value of number (99)
+                                // The value of number changes via pointer
+    
+      cout << &pNumber << endl; // Print the address of pointer variable pNumber (0x22ccec)
+}
---
+++ size_of.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 8;
+  cout << sizeof(x) << endl;
+  char letter = 'a';
+  cout << sizeof(letter);
+}
